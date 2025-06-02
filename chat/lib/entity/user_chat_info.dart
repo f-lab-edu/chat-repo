99cbec1d2c739511a5fbef3f5e8ec390c8e93fca
정재환 <jeongjaehwan@jeongjaehwan-ui-MacBookAir-M2.local>
2025-06-02 09:57:27 +0900

    modified user_chat_info

--- chat/lib/entity/user_chat_info.dart
+++ chat/lib/entity/user_chat_info.dart
@@ -4,7 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 // 추가: 채팅방 정보 모델 클래스
 class UserChatInfo {
   String? chatId; // 채팅방 고유 식별자
-  DateTime? lastQueryTime; // 사용자가 마지막으로 이 채팅방 정보를 조회한 시각
+  Timestamp? lastQueryTime; // 사용자가 마지막으로 이 채팅방 정보를 조회한 시각
 
   UserChatInfo({
     this.chatId,
@@ -13,7 +13,7 @@ class UserChatInfo {
 
   UserChatInfo.fromJson(Map<String, dynamic> json) {
     chatId = json['chatId'];
-    lastQueryTime = (json['lastQueryTime'] as Timestamp).toDate();
+    lastQueryTime = json['lastQueryTime'];
   }
 
   Map<String, dynamic> toJson() {
